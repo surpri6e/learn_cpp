d0aee1042b7b8aba8137da705154e4d638ff589e
surpri6e <dan.surpri6e@gmail.com>
2021-11-03 13:53:15 +0300

    hard task and my solution v-2

--- Task_2/vectorFeed.cpp
+++ Task_2/vectorFeed.cpp
@@ -15,30 +15,22 @@ int main() {
     uint64_t sizeArray = 0;
     cout << "Enter your number of size array: ";
     cin >> sizeArray;
-    if (sizeArray == 0 || sizeArray % 2 == 0 || sizeArray > 1000)
+
+    if (sizeArray == 0 || sizeArray % 2 == 0 || sizeArray > 100000) {
         cout << "Your number is incorrect!";
-    else if (sizeArray == 3) {
-        vector<int> arr(sizeArray);
-        for(int i = 0; i < sizeArray; i++)
-            arr[i] = i;
-        swap(arr[sizeArray - 1], arr[sizeArray - 2]);
-        print_vector(arr);
     }
     else {
         vector<int> arr(sizeArray);
-        int tmp_evenNumber = 1;
-        int tmp_oddNumber = sizeArray - 1;
-        for (int i = 0; i < sizeArray; i++) {
-            if (i == 0)
-                arr[i] = 0;
-            else if (i % 2 != 0)
+        uint64_t tmp_evenNumber = 1;
+        uint64_t tmp_oddNumber = sizeArray - 1;
+        for (int i = 1; i < sizeArray; i++) {
+            if(i % 2 != 0)
                 arr[i] = tmp_oddNumber--;
-            else
+            else if(i % 2 == 0)
                 arr[i] = tmp_evenNumber++;
         }
-    print_vector(arr);
+        print_vector(arr);
     }
-
 
     return 0;
 }
